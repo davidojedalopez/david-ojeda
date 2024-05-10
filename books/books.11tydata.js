8662b6ed0b81b3698925e5e05c84e81ab0b669c9
David Ojeda <david.ojeda.lopez@gmail.com>
2024-05-10 09:51:07 -0700

    Fix head tags

--- books/books.11tydata.js
+++ books/books.11tydata.js
@@ -1,11 +1,14 @@
 module.exports = {
   eleventyComputed: {
-      month: data => new Intl.DateTimeFormat('es-MX', {                        
-          month: 'long', 
+    month: (data) =>
+      new Intl.DateTimeFormat("es-MX", {
+        month: "long",
       }).format(new Date(data.read_at)),
-      year: data => new Intl.DateTimeFormat('es-MX', {                        
-          year: 'numeric' 
+    year: (data) =>
+      new Intl.DateTimeFormat("es-MX", {
+        year: "numeric",
       }).format(new Date(data.read_at)),
-      monthYear: data => `${data.month}, ${data.year}`
-  }
-}
+    monthYear: (data) => `${data.month}, ${data.year}`,
+    description: (data) => `Quotes and notes of ${data.title}`,
+  },
+};
